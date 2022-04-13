6079c7b9cbfdccc56eb46abd6a2b28d2017beea0
Luiz Gabriel Ribeiro <bieel180360@gmail.com>
2022-04-13 18:23:13 -0300

    Exercicio 6 aula 21

--- src/exerciciosA21/exercicio6.kt
+++ src/exerciciosA21/exercicio6.kt
@@ -1,3 +1,30 @@
 package exerciciosA21
 
-// WIP
+fun main(args: Array<String>) {
+    println("""
+        =======================
+          ANALISANDO MÊS ATUAL
+        =======================
+    """.trimIndent())
+
+    print("Qual valor você quer sacar? R$")
+    var valor: Double = readLine()!!.toDoubleOrNull()?:0.0
+
+    var ced50 = (valor / 50).toInt()
+    valor %= 50
+
+    var ced10 = (valor / 10).toInt()
+    valor %= 10
+
+    var ced2 = (valor / 2).toInt()
+    valor %= 2
+
+    var ced1 = (valor).toInt()
+
+    println("=========== RESULTADOS =============")
+    println("R$50,00 \t= $ced50 cédula(s)")
+    println("R$10,00 \t= $ced10 cédula(s)")
+    println("R$2,00  \t= $ced2 cédula(s)")
+    println("R$1,00  \t= $ced1 moeda(s)")
+    println("====================================")
+}
